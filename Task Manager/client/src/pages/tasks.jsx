8103e04f3923cc7493a86738da3bf4f13a167456
D4tLe <datlex3@gmail.com>
2025-03-26 00:36:36 +0700

    initialize project

---
+++ Task Manager/client/src/pages/tasks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const tasks = () => {
+  return (
+    <div>
+      Tasks
+    </div>
+  )
+}
+
+export default tasks
---
+++ Task Manager/client/src/pages/trash.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const trash = () => {
+  return (
+    <div>
+      Trash
+    </div>
+  )
+}
+
+export default trash
