bbd901b16431f45fcccc7c1876459a961949de47
PhamTuanAnh20225542 <anh.pt225542@sis.hust.edu.vn>
2025-03-31 15:55:50 +0700

    update login page

--- Task Manager/client/src/App.jsx
+++ Task Manager/client/src/App.jsx
@@ -1,9 +1,50 @@
 import Login from './pages/login'
+import Dashboard from './pages/dashboard'
+import Tasks from './pages/tasks'
+import Trash from './pages/trash'
+import Users from './pages/users'
+import TaskDetails from './pages/taskDetails'
+import { Routes, Route, Navigate, Outlet, useLocation } from 'react-router-dom'
+
+function Layout() {
+  const user = "";
+  const location = useLocation();
+  return user ? (
+    <div className='w-full h-screen flex flex-col md:flex-row'>
+      <div className='w-1/5 h-screen bg-white sticky top-0 hidden md:block'>
+        {/* Sidebar component here */}
+      </div>
+      {/* Mobile sidebar component here */}
+      <div className='flex-1 overflow-y-auto'>
+        {/* Navbar component here */}
+        <div className='p-4 2xl:px-10'>
+          <Outlet/>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Navigate to='/log-in' state={{ from: location }} replace/>
+  )
+}
 
 function App() {
-
   return (
-    <Login />
+    <main className='w-full min-h-screen bg-[#f3f4f6]'>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path='/' element={<Navigate to='/dashboard'/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/tasks' element={<Tasks/>}/>
+          <Route path='/completed/:status' element={<Tasks/>}/>
+          <Route path='/in-progress/:status' element={<Tasks/>}/>
+          <Route path='/todo/:status' element={<Tasks/>}/>
+          <Route path='/team' element={<Users/>}/>
+          <Route path='/trashed' element={<Trash/>}/>
+          <Route path='/task/:id' element={<TaskDetails/>}/>
+        </Route>
+        <Route path='/log-in' element={<Login/>}/>
+      </Routes>
+    </main>
   )
 }
 
