d50587b624f5ffe1f984ec4750ed469d99330775
D4tLe <datlex3@gmail.com>
2025-04-05 21:09:11 +0700

    update dashboard

--- Task Manager/client/src/App.jsx
+++ Task Manager/client/src/App.jsx
@@ -5,8 +5,14 @@ import Trash from './pages/trash'
 import Users from './pages/users'
 import TaskDetails from './pages/taskDetails'
 import { Routes, Route, Navigate, Outlet, useLocation } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Sidebar from './components/Sidebar'
+import Navbar from './components/Navbar'
+import { Fragment, useRef } from 'react'
+import { Button, Transition } from '@headlessui/react'
+import { IoClose } from 'react-icons/io5'
+import clsx from 'clsx'
+import { setOpenSidebar } from './redux/slices/api/authSlice'
 
 function Layout() {
   const { user } = useSelector(state => state.auth);
@@ -16,9 +22,9 @@ function Layout() {
       <div className='w-1/5 h-screen bg-white sticky top-0 hidden md:block'>
         <Sidebar />
       </div>
-      {/* Mobile sidebar component here */}
+      {<MobileSidebar />}
       <div className='flex-1 overflow-y-auto'>
-        {/* Navbar component here */}
+        <Navbar />
         <div className='p-4 2xl:px-10'>
           <Outlet/>
         </div>
@@ -28,6 +34,55 @@ function Layout() {
     <Navigate to='/login' state={{ from: location }} replace/>
   )
 }
+
+const MobileSidebar = () => {
+  const { isSidebarOpen } = useSelector((state) => state.auth);
+  const mobileMenuRef = useRef(null);
+  const dispatch = useDispatch();
+
+  const closeSidebar = () => {
+    dispatch(setOpenSidebar(false));
+  };
+
+  return (
+    <>
+      <Transition
+        show={isSidebarOpen}
+        as={Fragment}
+        enter='transition-opacity duration-700'
+        enterFrom='opacity-x-10'
+        enterTo='opacity-x-100'
+        leave='transition-opacity duration-700'
+        leaveFrom='opacity-x-100'
+        leaveTo='opacity-x-0'
+      >
+        {(ref) => (
+          <div
+            ref={(node) => (mobileMenuRef.current = node)}
+            className={`md:hidden w-full h-full bg-black/40 transition-transform duration-700 transform
+             ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}`}
+            onClick={() => closeSidebar()}
+          >
+            <div className='bg-white w-3/4 h-full'>
+              <div className='w-full flex justify-end px-5 pt-5'>
+                <button
+                  onClick={() => closeSidebar()}
+                  className='flex justify-end items-end'
+                >
+                  <IoClose size={25} />
+                </button>
+              </div>
+
+              <div className='-mt-10'>
+                <Sidebar />
+              </div>
+            </div>
+          </div>
+        )}
+      </Transition>
+    </>
+  );
+};
 
 function App() {
   return (
