c5d81b20c6ac1adbb572a32a66c6f103df9b0bd3
D4tLe <datlex3@gmail.com>
2025-04-30 00:03:53 +0700

    update taskdetails feature

--- Task Manager/client/src/assets/data.js
+++ Task Manager/client/src/assets/data.js
@@ -533,8 +533,8 @@ export const tasks = [
     priority: "high",
     stage: "todo",
     assets: [
-      "https://firebasestorage.googleapis.com/v0/b/taskmanager-557d7.appspot.com/o/1707471138863original-a005132062ca5bafc505c4c74f0e1865.jpg?alt=media&token=55f909f2-7f05-42f3-af4f-dc7f87cdea1d",
-      "https://firebasestorage.googleapis.com/v0/b/taskmanager-557d7.appspot.com/o/1707471144712PsZch9E1_400x400.jpg?alt=media&token=7ce62c7e-c240-4032-83c6-bb6c9cdc0d4b",
+      "https://res.cloudinary.com/imagist/image/fetch/q_auto,f_auto,c_scale,w_1536/https://www.todoist.com/static/home-teams/intro/wide/headerui.en.png",
+      "https://res.cloudinary.com/imagist/image/fetch/q_auto,f_auto,c_scale,w_1536/https://www.todoist.com/static/home-teams/intro/wide/headerui.en.png",
     ],
     team: [
       {
